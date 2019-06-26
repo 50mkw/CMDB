58e4448c989ece96b1f6b1ff9266f3968c416608
50mkw <patrick.yang@50mkw.com>
2019-06-26 22:15:59 +0800

    Initial commit

---
+++ Client/core/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""
+@Time        : 2019/6/26 4:47 PM
+@Author      : Patrick Yang
+@Email       : 490286710@qq.com
+@File        : __init__.py.py
+@Software    : PyCharm
+"""
+
